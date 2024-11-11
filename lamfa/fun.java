9aa3ccbe017d2dbd9571df97b6bdb8220779a668
coderamittad <amittad992004@gmail.com>
2024-11-12 02:02:34 +0530

    add files

---
+++ lamfa/fun.java
@@ -0,0 +1,7 @@
+package lamfa;
+
+public class fun {
+
+}
+
+
---
+++ lamfa/main.java
@@ -0,0 +1,12 @@
+package lamfa;
+
+public class main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+
+
+
+
+    }
+}
